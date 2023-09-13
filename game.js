86af31d9508d7635fc3ff1ccffeb161c0f035d0b
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-09-13 23:30:00 +0530

    ok so far

---
+++ game.js
@@ -0,0 +1,20 @@
+const {Client, Account,Database,ID,Query } = Appwrite
+const projectId = '6501c229d6ae06288249'
+const databaseId = ''
+const collectionId = ''
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(projectId)
+
+const account = new Account(client)
+
+function register(event) {
+    account.create(
+        ID.unique()
+        event.target.elements['register-email'].value,
+        event.target.elements['register-password'].value,
+        event.target.elements['register-username'].value,
+        
+    )
+    }
